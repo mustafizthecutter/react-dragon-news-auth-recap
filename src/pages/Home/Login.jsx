2100c6a489da37f29c7f3a7e21c6512760148178
Mustafizur Rahman <mustafizur232@gmail.com>
2024-01-10 20:28:44 +0600

    first commit

---
+++ src/pages/Home/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="text-2xl">this is login!!!</div>
+        </div>
+    );
+};
+
+export default Login;
